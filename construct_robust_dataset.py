ac46e175942509219afecb7de08396126fa99d2e
Anshuman <anshuman@email.virginia.edu>
2019-09-23 21:31:33 +0000

    TF implementation works (yet to check for correctness/visualize)

---
+++ construct_robust_dataset.py
@@ -0,0 +1,113 @@
+import tensorflow as tf
+import numpy as np
+
+import keras
+import keras.backend as K
+from keras.datasets import cifar10
+from keras.models import load_model, Model
+
+from tqdm import tqdm
+
+from cleverhans import utils_tf
+from cleverhans.utils_tf import clip_eta
+
+
+class CustomPGD:
+	def __init__(self, model, eps, eps_iter, nb_iter):
+		self.model = model
+		self.eps = eps
+		self.eps_iter = eps_iter
+		self.nb_iter = nb_iter
+		self.sess = K.get_session()
+
+	def modified_loss(self, target, x):
+		# Implement fake-relu (usecase???)
+		rep = self.model(x)
+		target_rep = self.model(target)
+		loss = tf.norm(rep - target_rep, ord='euclidean') / tf.norm(target_rep, ord='euclidean')
+		return loss
+
+	def tf_optimize_loop(self, x, x_r_seed):
+		adv_x = tf.identity(x_r_seed)
+
+		def cond(i, *_):
+			return tf.less(i, self.nb_iter)
+
+		def body(i, adv_x, best_advx, best_loss):
+			loss = self.modified_loss(x, adv_x)
+			grads,  = tf.gradients(loss, adv_x)
+
+			# Take step
+			scaled_grads = grads / (tf.norm(grads, ord='euclidean') + 1e-10)
+			adv_x = adv_x + scaled_grads * self.eps_iter
+
+			# Clipping perturbation eta to L-2 norm ball
+			eta = adv_x - x
+			eta = clip_eta(eta, 2, self.eps)
+			adv_x = tf.clip_by_value(x + eta, 0, 1)
+			
+			# Get current loss
+			(best_loss, best_advx)=  tf.cond(tf.less(loss, best_loss), lambda: (loss, adv_x), lambda: (best_loss, best_advx))
+
+			return i + 1, adv_x, best_advx, best_loss
+
+		_, _, best_adv_x, _ = tf.while_loop(cond, body, (tf.zeros([]), adv_x, adv_x, np.inf),
+										back_prop=True,
+										maximum_iterations=self.nb_iter)
+		return tf.identity(best_adv_x)
+
+	def find_optimal_datapoint(self, target, seed):
+		# sample initial x_adv from dataset
+		x        = tf.placeholder(tf.float32, shape=(None,) + tuple(target.shape))
+		x_r_seed = tf.placeholder(tf.float32, shape=(None,) + tuple(seed.shape))
+		adv_x = self.tf_optimize_loop(x, x_r_seed)
+		adv_x = self.sess.run(adv_x, feed_dict={x: np.expand_dims(target, 0), x_r_seed: np.expand_dims(seed, 0)})
+		return adv_x
+
+
+def sample_from_data(data):
+	choices = list(range(len(data)))
+	picked = np.random.choice(choices)
+	return data[picked]
+
+
+def construct_robust_dataset(model, data, sample_strategy):
+	pgd = CustomPGD(model, 0.1, 0.1, 1000)
+	robust_data = []
+	for x in tqdm(data):
+		x_seed = sample_strategy(data)
+		x_r = pgd.find_optimal_datapoint(x, x_seed)
+		robust_data.append(x_r)
+	return robust_data
+
+
+def cifar_to_robust(model):
+	feature_extractor = Model(model.inputs, model.layers[-3].output)
+	(X_train, _), (X_val, _) = cifar10.load_data()
+	X_train   = (X_train.astype('float32')) / 255
+	X_val     = (X_val.astype('float32')) / 255
+	robust_train = construct_robust_dataset(feature_extractor, X_train, sample_from_data)
+	robust_val   = construct_robust_dataset(feature_extractor, X_val, sample_from_data)
+	return (robust_train, robust_val)
+
+
+def create_and_save_robust_cifar(model, path):
+	(X_train, X_val) = cifar_to_robust(model)
+	np.savez(path, X_train=X_train, X_val=X_val)
+
+
+def load_robust_cifar(path):
+	data = np.load(path)
+	X_train, X_val = data['X_train'], data['X_val']
+	(_, y_train), (_, y_val) = cifar10.load_data()
+	return (X_train, y_train), (X_val, y_val)
+
+
+if __name__ == "__main__":
+	config = tf.compat.v1.ConfigProto()
+	config.gpu_options.allow_growth=True
+	session = tf.compat.v1.Session(config=config)
+	keras.backend.set_session(session)
+
+	model = load_model("./adversarialy_trained_final.h5")
+	create_and_save_robust_cifar(model, "./robust_cifar_data.npz")
--- train_robust.py
+++ train_robust.py
@@ -1,12 +1,16 @@
 from cleverhans.attacks import MadryEtAl
 from cleverhans.utils_keras import KerasModelWrapper
 
+from sklearn.utils import shuffle
+
 import keras
 from keras.datasets import cifar10
 
 import tensorflow as tf
 import numpy as np
 import sys
+
+import imgaug.augmenters as iaa
 
 import models
 
@@ -22,19 +26,30 @@ attack = MadryEtAl
 attack_params = {'clip_min': 0, 'clip_max': 1}
 attack_object = attack(wrap, sess=session)
 attack_params['nb_iter'] = 7
-attack_params['eps'] =5e-1
+attack_params['eps'] = 5e-1
+attack_params['eps_iter'] = 5e-1 / 5
 
 def raw_to_train(x):
+	return x / 255
 	# mean = [0.4914, 0.4822, 0.4465]
 	# var =  [0.2023, 0.1994, 0.2010]
-	# return (x - mean) / var
-	return x / 255
+	# return ((x/255) - mean) / var
 
 def train_to_raw(x):
+	return 255 * x
 	# mean = [0.4914, 0.4822, 0.4465]
 	# var =  [0.2023, 0.1994, 0.2010]
-	# return x * var + mean
-	return x * 255
+	# return 255*((x * var) + mean)
+
+def get_image_augmentations():
+	seq = iaa.Sequential([
+		iaa.Pad(4), # Pad all sides by 4
+    	iaa.CropToFixedSize(32, 32), # Crop to fixed size
+    	iaa.Fliplr(0.1), # horizontally flip 10% of the images
+    	iaa.Affine(rotate=(-2, 2)) # Rotate by 2 degrees
+	])
+	return seq
+
 
 (X_train, Y_train), (X_val, Y_val) = cifar10.load_data()
 X_train = X_train.astype('float32')
@@ -44,24 +59,31 @@ X_val   = raw_to_train(X_val)
 Y_train = keras.utils.to_categorical(Y_train, 10)
 Y_val   = keras.utils.to_categorical(Y_val, 10)
 
+# Shuffle data
+X_train, Y_train = shuffle(X_train, Y_train)
+
 nb_epochs = 150
-batch_size = 128
+batch_size = 128 // (2 * 2)
 
 init = tf.global_variables_initializer()
 session.run(init)
+
+augmentor = get_image_augmentations()
 
 for i in range(nb_epochs):
 	batch_no = 1
 	train_loss, train_acc = 0, 0
 	for j in range(0, len(X_train), batch_size):
 		x_clean, y_clean = X_train[j:j+batch_size], Y_train[j:j+batch_size]
-		x_adv = attack_object.generate_np(x_clean, **attack_params)
-		x_batch = np.concatenate([x_clean, x_adv], axis=0)
-		y_batch = np.concatenate([y_clean, y_clean], axis=0)
+		x_aug = augmentor.augment_images(x_clean)
+		x_clean_use = np.concatenate([x_clean, x_aug], axis=0)
+		x_adv = attack_object.generate_np(x_clean_use, **attack_params)
+		x_batch = np.concatenate([x_clean_use, x_adv], axis=0)
+		y_batch = np.concatenate([y_clean, y_clean, y_clean, y_clean], axis=0)
 		train_metrics = model.train_on_batch(x_batch, y_batch)
 		train_loss += train_metrics[0]
 		train_acc += train_metrics[1]
-		sys.stdout.write("Epoch %d: %d / %d : Tr loss: %f, Tr acc: %f  \r" % (i+1, batch_no, len(X_train)//batch_size, train_loss/(batch_no), train_acc/(batch_no)))
+		sys.stdout.write("Epoch %d: %d / %d : Tr loss: %f, Tr acc: %f  \r" % (i+1, batch_no, 1 + len(X_train)//batch_size, train_loss/(batch_no), train_acc/(batch_no)))
 		sys.stdout.flush()
 		batch_no += 1
 	val_metrics = model.evaluate(X_val, Y_val, batch_size=1024, verbose=0)
@@ -70,4 +92,4 @@ for i in range(nb_epochs):
 	if i % 10 == 9:
 		model.save("adversarialy_trained_%d.h5" % (i + 1))
 
-model.save("adversarialy_trained_final.h5" % (i + 1))
+model.save("adversarialy_trained_final.h5")
