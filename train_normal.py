3d449c0bc8c9effa0611a47cbd04c2f7b4f87cad
Anshuman <anshuman@email.virginia.edu>
2019-09-25 18:27:55 +0000

    Modularise code slightly

--- train_normal.py
+++ train_normal.py
@@ -5,6 +5,7 @@ from keras.datasets import cifar10
 
 import tensorflow as tf
 import numpy as np
+import argparse
 import sys
 
 import imgaug.augmenters as iaa
@@ -14,12 +15,21 @@ from construct_robust_dataset import load_robust_cifar
 
 import matplotlib
 
-config = tf.compat.v1.ConfigProto()
-config.gpu_options.allow_growth=True
-session = tf.compat.v1.Session(config=config)
-keras.backend.set_session(session)
+parser = argparse.ArgumentParser()
+parser.add_argument('-b','--batch_size', type=int, default=128, metavar='NUMBER', help='batch size(default: 128)')
+parser.add_argument('-e','--nb_epochs', type=int, default=200, metavar='NUMBER', help='epochs(default: 200)')
+parser.add_argument('-g','--save_here', type=str, default="", metavar='STRING', help='path where trained model should be saved')
+parser.add_argument('-a','--augment', type=bool, default=False, metavar='BOOLEAN', help='use augmentation while training data')
+parser.add_argument('-r','--robust_data', type=bool, default=True, metavar='BOOLEAN', help='use robust data?')
+args = parser.parse_args()
 
-model, cbks = models.ResNet50(input_shape=(32, 32, 3), classes=10)
+
+def conserve_gpu_memory():
+	config = tf.compat.v1.ConfigProto()
+	config.gpu_options.allow_growth=True
+	session = tf.compat.v1.Session(config=config)
+	keras.backend.set_session(session)
+
 
 def raw_to_train(x):
 	return x / 255
@@ -60,34 +70,48 @@ def load_robust_data():
 	return (X_train, Y_train), (X_val, Y_val)
 
 
-(X_train, Y_train), (X_val, Y_val) = load_robust_data()
+def train_model(batch_size, nb_epochs, augment, save_path, use_robust):
+	model, cbks = models.ResNet50(input_shape=(32, 32, 3), classes=10)
 
-nb_epochs = 200
-batch_size = 128 // 2
+	session = keras.backend.get_session()
+	init = tf.global_variables_initializer()
+	session.run(init)
 
-init = tf.global_variables_initializer()
-session.run(init)
+	if use_robust:
+		(X_train, Y_train), (X_val, Y_val) = load_robust_data()
+	else:
+		(X_train, Y_train), (X_val, Y_val) = load_normal_data()
 
-augmentor = get_image_augmentations()
+	if augment:
+		batch_size //= 2
+		augmentor = get_image_augmentations()
 
-for i in range(nb_epochs):
-	batch_no = 1
-	train_loss, train_acc = 0, 0
-	for j in range(0, len(X_train), batch_size):
-		x_clean, y_clean = X_train[j:j+batch_size], Y_train[j:j+batch_size]
-		x_aug = augmentor.augment_images(x_clean)
-		x_batch = np.concatenate([x_clean, x_aug], axis=0)
-		y_batch = np.concatenate([y_clean, y_clean], axis=0)
-		train_metrics = model.train_on_batch(x_batch, y_batch)
-		train_loss += train_metrics[0]
-		train_acc += train_metrics[1]
-		sys.stdout.write("Epoch %d: %d / %d : Tr loss: %f, Tr acc: %f  \r" % (i+1, batch_no, 1 + len(X_train)//batch_size, train_loss/(batch_no), train_acc/(batch_no)))
-		sys.stdout.flush()
-		batch_no += 1
-	val_metrics = model.evaluate(X_val, Y_val, batch_size=1024, verbose=0)
-	print()
-	print(">> Val loss: %f, Val acc: %f"% (val_metrics[0], val_metrics[1]))
-	if i % 10 == 9:
-		model.save("./models/normally_trained_%d.h5" % (i + 1))
+	for i in range(nb_epochs):
+		batch_no = 1
+		train_loss, train_acc = 0, 0
+		for j in range(0, len(X_train), batch_size):
+			x_clean, y_clean = X_train[j:j+batch_size], Y_train[j:j+batch_size]
+			if augment:
+				x_aug = augmentor.augment_images(x_clean)
+				x_batch = np.concatenate([x_clean, x_aug], axis=0)
+				y_batch = np.concatenate([y_clean, y_clean], axis=0)
+			else:
+				x_batch, y_batch = x_clean, y_clean
+			train_metrics = model.train_on_batch(x_batch, y_batch)
+			train_loss += train_metrics[0]
+			train_acc += train_metrics[1]
+			sys.stdout.write("Epoch %d: %d / %d : Tr loss: %f, Tr acc: %f  \r" % (i+1, batch_no, 1 + len(X_train)//batch_size, train_loss/(batch_no), train_acc/(batch_no)))
+			sys.stdout.flush()
+			batch_no += 1
+		val_metrics = model.evaluate(X_val, Y_val, batch_size=1024, verbose=0)
+		print()
+		print(">> Val loss: %f, Val acc: %f"% (val_metrics[0], val_metrics[1]))
+		if i % 10 == 9:
+			model.save(save_path + "_%d.h5" % (i + 1))
 
-model.save("./models/normally_trained_final.h5")
+	model.save(save_path +  "_final.h5")
+
+
+if __name__ == "__main__":
+	conserve_gpu_memory()
+	train_model(args.batch_size, args.nb_epochs, args.augment, args.save_here, args.robust_data)
