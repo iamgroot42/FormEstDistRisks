Fix GCN mean degree calculation while pruning

boneage/data_utils.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
+import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
 import torch as ch
@@ -145,7 +146,9 @@ if __name__ == "__main__":
     # For the sake of varying property ratios, both use
     # Samples from their respective splits
     # Same goes for train-val splits
-    df_victim, df_adv = process_data(base)
+
+    # Make a 2:1 victim:adv split
+    df_victim, df_adv = process_data(base, split_second_ratio=0.33)
 
     # Save these splits
     def save_split(df, split):

gcn/data_utils.py
@@ -52,7 +52,7 @@ class GraphData:
 
         # Make bidirectional
         self.g = dgl.to_bidirected(self.g)
-    
+
     def shift_to_gpu(self):
         # Shift graph, labels to cuda
         self.g = self.g.to('cuda')
@@ -88,7 +88,7 @@ class ArxivNodeDataset(GraphData):
     def __init__(self, split, normalize=True):
         super(ArxivNodeDataset, self).__init__(
             'ogbn-arxiv', normalize=normalize)
-        
+
         # 59:41 victim:adv data split (all original data, including train/val/test)
         # Original data had 54:46 train-nontrain split
         # Get similar splits
@@ -129,23 +129,49 @@ class ArxivNodeDataset(GraphData):
         n_elems = len(self.train_idx)
         perm = ch.randperm(n_elems)[:int(ratio * n_elems)]
         self.train_idx = self.train_idx[perm]
-    
-    def change_mean_degree(self, wanted_degree, shuffle=False):
+
+    def label_ratio_preserving_pick(self, total_tr, total_te):
+        # While maintaining relative label ratios for classes
+        # Sample a set of size total
+        labels = ch.cat(
+            (self.labels[self.train_idx, 0], self.labels[self.test_idx, 0]), 0)
+        elems, counts = ch.unique(labels, return_counts=True)
+        # Get ratios for these elems
+        counts = counts.float() / len(labels)
+        # Sample according to ratio from existing train, test sets
+        train_new, test_new = [], []
+        for e, c in zip(elems, counts):
+            # for train
+            qualify = ch.nonzero(
+                self.labels[self.train_idx, 0] == e, as_tuple=True)[0]
+            ids = ch.randperm(len(qualify))[:int(c * total_tr)]
+            train_new.append(qualify[ids])
+            # for test
+            qualify = ch.nonzero(
+                self.labels[self.test_idx, 0] == e, as_tuple=True)[0]
+            ids = ch.randperm(len(qualify))[:int(c * total_te)]
+            test_new.append(qualify[ids])
+
+        self.train_idx = ch.cat(train_new, 0)
+        self.test_idx = ch.cat(test_new, 0)
+
+    def change_mean_degree(self, wanted_degree):
         # If no change requested, perform no change
-        if wanted_degree is None: return 
+        if wanted_degree is None:
+            return
 
         # Compute degrees
         self.precompute_degrees()
 
         # Prune graph, get rid of nodes
         self.g, pruned_nodes = achieve_mean_degree(
-            self.g, self.degs, wanted_degree, shuffle)
+            self.g, self.degs, wanted_degree)
 
         # Make mapping between old and new IDs
         not_pruned = ch.ones(self.num_nodes).byte()
         not_pruned[pruned_nodes] = False
         not_pruned = ch.nonzero(not_pruned, as_tuple=True)[0]
-        mapping = {x.item():i for i, x in enumerate(not_pruned)}
+        mapping = {x.item(): i for i, x in enumerate(not_pruned)}
 
         # Function to modify current masks to reflect pruning
         def process(ids):
@@ -163,11 +189,11 @@ class ArxivNodeDataset(GraphData):
             # Update current mask to point to correct IDs
             for i, x in enumerate(keep):
                 keep[i] = mapping[x.item()]
-            
+
             # Shift mask back to GPU
             keep = keep.cuda()
             return keep
-        
+
         # Update masks to account for pruned nodes,  re-indexing
         self.train_idx = process(self.train_idx)
         self.test_idx = process(self.test_idx)
@@ -181,46 +207,63 @@ class ArxivNodeDataset(GraphData):
         self.num_nodes -= len(pruned_nodes)
 
 
-def find_to_prune(degs, wanted_deg, shuffle=False):
+def find_to_prune(g, degs, wanted_deg):
     prune = []
     # Take note of mean degree right now
     cur_deg = np.mean(degs)
-    # If desired degree is more than current, prune low-degree nodes
-    i = 0
-    if wanted_deg > cur_deg:
-        # Find sorted order for degrees
-        so = np.argsort(degs)
-        if shuffle:
-            # Shuffle first 10% of to-remove data
-            # To add some randomness
-            bp = int(0.1 * len(so))
-            so[:bp] = so[ch.randperm(bp)]
 
+    # If desired degree is more than current, prune low-degree nodes
+    if wanted_deg > cur_deg:
+
+        inf_val = np.max(degs) + 1
         while cur_deg < wanted_deg:
-            i += 1
-            cur_deg = np.mean(degs[so[i:]])
-            prune.append(so[i])
+            # Find minimum right now
+            pick = np.argmin(degs)
+
+            # Reduce degree of all nodes that were connected to pruned node
+            # And are up for consideration currently
+            L, R = g.edges()
+            neighbors = R[L == pick].cpu().numpy()
+            neighbors_mask = np.zeros_like(degs, dtype=bool)
+            neighbors_mask[neighbors] = True
+            neighbors_mask[degs == inf_val] = False
+            degs[neighbors_mask] -= 1
+
+            # Removed this node, mark as INF
+            degs[pick] = inf_val
+
+            cur_deg = np.mean(degs[degs != inf_val])
+            prune.append(pick)
+
     # Else, prune high-degree nodes
     else:
-        # Find reverse-sorted order for degrees
-        so = np.argsort(-degs)
-        if shuffle:
-            # Shuffle first 10% of to-remove data
-            # To add some randomness
-            bp = int(0.1 * len(so))
-            so[:bp] = so[ch.randperm(bp)]
 
+        inf_val = -1
         while cur_deg > wanted_deg:
-            i += 1
-            cur_deg = np.mean(degs[so[i:]])
-            prune.append(so[i])
+            # Find minimum right now
+            pick = np.argmax(degs)
+
+            # Reduce degree of all nodes that were connected to pruned node
+            # And are up for consideration currently
+            L, R = g.edges()
+            neighbors = R[L == pick].cpu().numpy()
+            neighbors_mask = np.zeros_like(degs, dtype=bool)
+            neighbors_mask[neighbors] = True
+            neighbors_mask[degs == inf_val] = False
+            degs[neighbors_mask] -= 1
+
+            # Removed this node, mark as -1
+            degs[pick] = inf_val
+
+            cur_deg = np.mean(degs[degs != inf_val])
+            prune.append(pick)
     # Return list of nodes that should be removed
     return prune
 
 
-def achieve_mean_degree(g, degs, wanted_deg, shuffle=False):
+def achieve_mean_degree(g, degs, wanted_deg):
     # Find which nodes should be pruned
-    to_prune = find_to_prune(degs, wanted_deg, shuffle)
+    to_prune = find_to_prune(g, degs, wanted_deg)
 
     # Get rid of these nodes from graph
     g = dgl.remove_nodes(g, to_prune)

gcn/meta.py
@@ -14,6 +14,7 @@ def get_model_features(model_dir, ds, args, max_read=None):
     vecs = []
     iterator = os.listdir(model_dir)
     if max_read is not None:
+        np.random.shuffle(iterator)
         iterator = iterator[:max_read]
 
     for mpath in tqdm(iterator):
@@ -102,17 +103,17 @@ def main():
     ds = ArxivNodeDataset('adv')
 
     # Directories where saved models are stored
-    train_dir_1 = "models/adv/og"
-    train_dir_2 = "models/adv/deg10"
-    test_dir_1 = "models/victim/og"
-    test_dir_2 = "models/victim/deg10"
+    train_dir_1 = "models/adv/deg13"
+    train_dir_2 = "models/adv/deg11"
+    test_dir_1 = "models/victim/deg13"
+    test_dir_2 = "models/victim/deg11"
 
     # Load models, convert to features
-    dims, vecs_train_1 = get_model_features(train_dir_1, ds, args)
-    _, vecs_train_2 = get_model_features(train_dir_2, ds, args)
+    dims, vecs_train_1 = get_model_features(train_dir_1, ds, args, max_read=700)
+    _, vecs_train_2 = get_model_features(train_dir_2, ds, args, max_read=700)
 
-    _, vecs_test_1 = get_model_features(test_dir_1, ds, args, max_read=500)
-    _, vecs_test_2 = get_model_features(test_dir_2, ds, args, max_read=500)
+    _, vecs_test_1 = get_model_features(test_dir_1, ds, args)
+    _, vecs_test_2 = get_model_features(test_dir_2, ds, args)
 
     # Ready train, test data
     Y_train = [0.] * len(vecs_train_1) + [1.] * len(vecs_train_2)
@@ -129,8 +130,8 @@ def main():
     train_model(metamodel,
                 (X_train, Y_train),
                 (X_test, Y_test),
-                epochs=150,
-                eval_every=10)
+                epochs=40,
+                eval_every=5)
 
 
 if __name__ == "__main__":

implems/celeba_act_distrs.py
@@ -6,6 +6,8 @@ import numpy as np
 import torch.nn as nn
 import utils
 import implem_utils
+import itertools
+
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -13,34 +15,38 @@ mpl.rcParams['figure.dpi'] = 200
 
 
 if __name__ == "__main__":
-    batch_size = 250 * 4
-    # batch_size = 250 * 4
+    batch_size = 250 * 3
     # batch_size = 500 * 3
-    # batch_size = 500 * 4
 
     paths = [
         # First ratio category
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/1/"
-        # "13_0.9135180520570949.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/casia/1/"
-        # "15_0.9125104953820319.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/vggface/1/"
-        # "15_0.9222502099076406.pth",
-        "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/2/"
-        "15_0.9177162048698573.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/3/"
-        # "15_0.9136859781696054.pth",
+        [
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/1/"
+            "13_0.9135180520570949.pth",
+            # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/casia/1/"
+            # "15_0.9125104953820319.pth",
+            # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/vggface/1/"
+            # "15_0.9222502099076406.pth",
+            # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/2/"
+            # "15_0.9177162048698573.pth",
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/3/"
+            "15_0.9136859781696054.pth",
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/all/none/2/"
+            "15_0.9177162048698573.pth",
+        ],
         # Second ratio category
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/1/"
-        # "15_0.9122836498067551.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/casia/1/"
-        # "15_0.9107712989413544.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/vggface/1/"
-        # "15_0.9121156108217107.pth",
-        "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/2/"
-        "15_0.9122836498067551.pth",
-        # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/3/"
-        # "12_0.9137960006721559.pth",
+        [
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/1/"
+            "15_0.9122836498067551.pth",
+            # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/casia/1/"
+            # "15_0.9107712989413544.pth",
+            # "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/vggface/1/"
+            # "15_0.9121156108217107.pth",
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/2/"
+            "15_0.9122836498067551.pth",
+            "/p/adversarialml/as9rw/celeb_models/50_50/split_1/male/none/3/"
+            "12_0.9137960006721559.pth",
+        ]
     ]
 
     # Use existing dataset instead
@@ -58,81 +64,89 @@ if __name__ == "__main__":
     target_attr = attrs.index("Smiling")
     focus_value = 1
 
-    distrs = []
-    for MODELPATH in paths:
+    colors = ['C0', 'C1']
+    hatches = ['*', 'o', 'x']
+    labels = [[]] * len(paths)
 
-        model = utils.FaceModel(512,
-                                train_feat=True,
-                                weight_init=None).cuda()
-        model = nn.DataParallel(model)
-        model.load_state_dict(ch.load(MODELPATH), strict=False)
-        model.eval()
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111, projection='3d')
 
-        all_acts, acts = [], []
-        for x, y in tqdm(dataloader):
-            # wanted = ch.logical_and(y[:, focus_attr] == focus_value,
-            #                         y[:, target_attr] == 0)
-            wanted = y[:, focus_attr] == focus_value
-            # latent = model(x[wanted].cuda(), only_latent=True).detach()
-            # latent = model(x[wanted].cuda(), only_latent=True,
-            # latent = model(x.cuda(), only_latent=True).detach()
-            latent = model(x.cuda(), only_latent=True,
-                           deep_latent=9,
-                           within_block=None).detach()
-            latent = latent.view(latent.shape[0], -1)
-            num_dims = latent.shape[1]
-            act = ch.sum(latent > 0, 1).cpu().numpy()
-            # acts.append(act)
-            acts.append(act[wanted])
-            all_acts.append(act)
+    for i in range(len(paths)):
+        labels[i] = [r'$M^{%d}_{%d}$' % (j, i) for j in range(len(paths[i]))]
+    for j, mtypes in enumerate(paths):
+        distrs = []
+        for MODELPATH in mtypes:
 
-        all_acts = np.concatenate(all_acts)
-        # Mean-centering distribution values per model
-        median_count = int(np.median(all_acts))
-        # distrs.append(np.concatenate(acts) - median_count)
-        # [0-1] based on minimum-maximum activations across all data
-        minc, maxc = np.min(all_acts), np.max(all_acts)
-        # distrs.append((np.concatenate(acts) - minc) / (maxc - minc))
-        mean, std = np.mean(all_acts), np.std(all_acts)
-        # distrs.append((np.concatenate(acts) - mean) / std)
+            model = utils.FaceModel(512,
+                                    train_feat=True,
+                                    weight_init=None).cuda()
+            model = nn.DataParallel(model)
+            model.load_state_dict(ch.load(MODELPATH), strict=False)
+            model.eval()
 
-        # Data-agnostic normalizaion to [0, 1]
-        distrs.append(np.concatenate(acts) / num_dims)
-        print("median:", median_count)
-        print("min, max:", minc, maxc)
-        print("mean, std:", mean, std)
-        print()
+            all_acts, acts = [], []
+            for x, y in tqdm(dataloader):
+                # wanted = ch.logical_and(y[:, focus_attr] == focus_value,
+                #                         y[:, target_attr] == 0)
+                wanted = y[:, focus_attr] == focus_value
+                # latent = model(x[wanted].cuda(), only_latent=True).detach()
+                # latent = model(x[wanted].cuda(), only_latent=True,
+                latent = model(x.cuda(), only_latent=True).detach()
+                # latent = model(x.cuda(), only_latent=True,
+                #                deep_latent=3,
+                #                within_block=None).detach()
+                latent = latent.view(latent.shape[0], -1)
+                num_dims = latent.shape[1]
+                act = ch.sum(latent > 0, 1).cpu().numpy()
+                # acts.append(act)
+                acts.append(act[wanted])
+                all_acts.append(act)
 
-    labels = [
-        "45% males",
-        # "45% males",
-        # "45% males",
-        "67% males",
-        # "67% males",
-        # "67% males"
-    ]
+            all_acts = np.concatenate(all_acts)
+            # Mean-centering distribution values per model
+            median_count = int(np.median(all_acts))
+            # distrs.append(np.concatenate(acts) - median_count)
+            # [0-1] based on minimum-maximum activations across all data
+            minc, maxc = np.min(all_acts), np.max(all_acts)
+            # distrs.append((np.concatenate(acts) - minc) / (maxc - minc))
+            mean, std = np.mean(all_acts), np.std(all_acts)
+            # distrs.append((np.concatenate(acts) - mean) / std)
 
-    colors = [
-        'C0', #'C0', 'C0',
-        'C1', #'C1', 'C1'
-    ]
+            # Data-agnostic normalizaion to [0, 1]
+            distrs.append(np.concatenate(acts) / num_dims)
+            print("median:", median_count)
+            print("min, max:", minc, maxc)
+            print("mean, std:", mean, std)
+            print()
 
-    hatches = [
-        'o', #'x', '.',
-        'o', #'x', '.'
-    ]
+        for i, dist in enumerate(distrs):
 
-    for i, dist in enumerate(distrs):
-        plt.hist(dist,
-                 bins=50,
-                 density=True,
-                 alpha=0.75,
-                 label=labels[i],
-                 color=colors[i],
-                 hatch=hatches[i])
+            yspacing = 1
+            hist, bin_edges = np.histogram(dist, bins=50, density=True)
+            dx = np.diff(bin_edges)
+            dy = np.ones_like(hist)
+            y = (j * len(labels[j]) + i) * (1 + yspacing) * np.ones_like(hist)
+            z = np.zeros_like(hist)
+
+            # ax.bar3d(bin_edges[:-1], y, z, dx, dy, hist, color=colors[j],
+            #          zsort='average', alpha=0.75, label=labels[j][i])
+
+            plt.hist(dist,
+                     bins=50,
+                     density=True,
+                     alpha=0.6,
+                     label=labels[j][i],
+                     color=colors[j],
+                     hatch=hatches[i])
 
     plt.legend()
-    plt.title("Activation trends on male (block 9)")
-    plt.xlabel("Number of neurons activated")
+    plt.title(r"Activation distributions on $x\mid f(x)=1$")
+    plt.xlabel("Fraction of neurons activated")
     plt.ylabel("Normalized frequency of samples")
+
+    # ax.set_xlabel("Fraction of neurons activated")
+    # ax.set_ylabel(r"Models $M_i^j$")
+    # ax.set_zlabel("Normalized frequency of samples")
+    # ax.set_yticklabels(list(itertools.chain(*labels)))
+
     plt.savefig("../visualize/act_distr_male.png")