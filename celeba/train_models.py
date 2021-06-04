744f23150e3117a13ce8ad438ba398fa62b883f2
Anshuman Suri <anshuman@email.virginia.edu>
2021-06-04 15:21:50 -0400

    Activation met-classifier, adding ENV

--- celeba/train_models.py
+++ celeba/train_models.py
@@ -1,4 +1,4 @@
-from model_utils import create_model, save_model, VGG11BN
+from model_utils import create_model, save_model
 from data_utils import SUPPORTED_PROPERTIES, CelebaWrapper
 from utils import flash_utils, train
 
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', required=True,
                         help='filename (prefix) to save model')
-    parser.add_argument('--epochs', type=int, default=20,
+    parser.add_argument('--epochs', type=int, default=5,
                         help='number of epochs to train model for')
     parser.add_argument('--filter', help='alter ratio for this attribute',
                         required=True, choices=SUPPORTED_PROPERTIES)
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                         help='desired ratio for attribute')
     parser.add_argument('--split', choices=['victim', 'adv'], required=True)
     parser.add_argument('--bs', type=int, default=128, help='batch size')
-    parser.add_argument('--lr', type=float, default=1e-3, help='learning rate')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
     parser.add_argument('--weightinit', type=str, default='vggface2',
                         help='which weight initialization to use: vggface2, " \
                             "casia-webface, or none')
@@ -24,9 +24,6 @@ if __name__ == "__main__":
                         help='use data augmentations when training models?')
     parser.add_argument('--parallel', action="store_true",
                         help='use multiple GPUs to train?')
-    parser.add_argument('--hidden', type=str, default="64,16",
-                        help='comma-separated dimensions for hidden layers " \
-                        "for models classification layer')
     args = parser.parse_args()
     flash_utils(args)
 
@@ -38,15 +35,7 @@ if __name__ == "__main__":
     train_loader, test_loader = ds.get_loaders(args.bs)
 
     # Create model
-    # hidden_layer_sizes = [int(x) for x in args.hidden.split(",")]
-    # model = create_model(512, hidden=hidden_layer_sizes,
-    #                      weight_init=args.weightinit,
-    #                      parallel=args.parallel)
-
-    # Nope, use VGG
-    import torch.nn as nn
-    model = VGG11BN().cuda()
-    model = nn.DataParallel(model)
+    model = create_model(parallel=args.parallel)
 
     # Train model
     vloss, vacc = train(model, (train_loader, test_loader),
--- census/census_acts_metaclassifier.py
+++ census/census_acts_metaclassifier.py
@@ -1,11 +1,12 @@
 from model_utils import get_models_path, load_model
+from sklearn.ensemble import RandomForestClassifier
 from data_utils import CensusWrapper, SUPPORTED_PROPERTIES
 import model_utils
 import numpy as np
 from tqdm import tqdm
 import os
 import argparse
-from utils import get_z_value, get_threshold_acc, find_threshold_acc, flash_utils
+from utils import flash_utils
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import model_utils
@@ -23,14 +24,17 @@ def get_models(folder_path, n_models=100):
     return models
 
 
-def get_acts(data, models, colors, ratio):
-    x_te, y_te = data
-    all_acts = [] #Will represent the activation count corresponding to each layer in 1 model
+def get_acts(data, models, model_data, ratio, mask=None):
+    x_te, _ = data
+    if mask is not None:
+        x_te = x_te[mask]
     for model in tqdm(models):
-        acts = model_utils.layer_output(x_te[0:99], model, layer = 0) #change to layer = 0,1, or 2
-        acts1 = model_utils.layer_output(x_te[0:99], model, layer = 1) #change to layer = 0,1, or 2
-        acts2 = model_utils.layer_output(x_te[0:99], model, layer = 2) #change to layer = 0,1, or 2
-        print(acts)
+        # Will represent the activation count corresponding to each layer in 1 model
+        all_acts = []
+        acts = model_utils.layer_output(x_te, model, layer=1)
+        acts1 = model_utils.layer_output(x_te, model, layer=2)
+        acts2 = model_utils.layer_output(x_te, model, layer=3)
+
         activationCount = np.sum(acts > 0, 1)
         activationCount1 = np.sum(acts1 > 0, 1)
         activationCount2 = np.sum(acts2 > 0, 1)
@@ -39,33 +43,29 @@ def get_acts(data, models, colors, ratio):
         all_acts.append(activationCount1)
         all_acts.append(activationCount2)
 
-        #plt.plot(x, all_acts, color = colors)
         model_data[0].append(np.concatenate(all_acts))
         model_data[1].append(ratio)
 
-        all_acts = []
-
-    return np.array(acts), ratio
-
 
 if __name__ == "__main__":
-    #Example command: python census_acts.py --filter sex --ratio_1 0.5 --ratio_2 1.0
+    # Example command: python census_acts.py --filter sex --ratio_1 0.5 --ratio_2 1.0
     parser = argparse.ArgumentParser()
     parser.add_argument('--filter', choices=SUPPORTED_PROPERTIES,
+                        required=True,
                         help='name for subfolder to save/load data from')
     parser.add_argument('--ratio_1', help="ratio for D_1", default="0.5")
-    parser.add_argument('--ratio_2', help="ratio for D_2")
+    parser.add_argument('--ratio_2', help="ratio for D_2", required=True)
     args = parser.parse_args()
     flash_utils(args)
 
     # Get victim models
-    #models_victim_1 = get_models(
-    #    get_models_path(args.filter, "victim", args.ratio_1))
-    #models_victim_2 = get_models(
-    #    get_models_path(args.filter, "victim", args.ratio_2))
+    models_victim_1 = get_models(
+       get_models_path(args.filter, "victim", args.ratio_1))
+    models_victim_2 = get_models(
+       get_models_path(args.filter, "victim", args.ratio_2))
 
     # Load adv models
-    total_models = 100
+    total_models = 200
     models_1 = get_models(get_models_path(
         args.filter, "victim", args.ratio_1), total_models // 2)
     models_2 = get_models(get_models_path(
@@ -80,7 +80,8 @@ if __name__ == "__main__":
         ratio=float(args.ratio_2), split="adv")
 
     # Fetch test data from both ratios
-    _, (x_te_1, y_te_1), _ = ds_2.load_data(custom_limit=10000) #set both to ds_1 or ds_2 for current experiment
+    _, (x_te_1, y_te_1), _ = ds_2.load_data(custom_limit=10000)
+    # set both to ds_1 or ds_2 for current experiment
     _, (x_te_2, y_te_2), _ = ds_2.load_data(custom_limit=10000)
     y_te_1 = y_te_1.ravel()
     y_te_2 = y_te_2.ravel()
@@ -88,27 +89,72 @@ if __name__ == "__main__":
     # Iterate through data from both distrs
     z_vals, f_accs = [], []
     loaders = [(x_te_1, y_te_1), (x_te_2, y_te_2)]
-    allaccs_1, allaccs_2 = [], []
 
-    clf = model_utils.get_model()
-    model_data = [[], []] #Data that will store x and y values
+    # First train classifier using ALL test data
+    clf = RandomForestClassifier(max_depth=3)
+    #Data that will store x and y values
+    model_data = [[], []]
 
     for loader in loaders:
-        
         # Load models and get/plot activations
-        allaccs_1.append(get_acts(loader, models_1, colors = "orangered", ratio = args.ratio_1)) 
-        allaccs_2.append(get_acts(loader, models_2, colors = "yellowgreen", ratio = args.ratio_2))
-    
+        get_acts(loader, models_1, model_data, ratio=args.ratio_1)
+        get_acts(loader, models_2, model_data, ratio=args.ratio_2)
+
+    model_data[0] = np.array(model_data[0])
+    model_data[1] = np.array(model_data[1])
+
     x_tr = model_data[0]
-    y_tr = model_data[1]
-
+    y_tr = (model_data[1] != args.ratio_1)
     clf.fit(x_tr, y_tr)
-    
-    print(model_data)
     train_acc = 100 * clf.score(x_tr, y_tr)
+    print("Accuracy using all data", train_acc)
 
-    print(train_acc)
+    # Then, look at feature importances used by the model
+    # And pick the top 100 points
+    n_points = len(clf.feature_importances_) // 3
+    point_wise_importances = [
+        clf.feature_importances_[:n_points],
+        clf.feature_importances_[n_points:2*n_points],
+        clf.feature_importances_[n_points*2:]
+    ]
+    # Account all activations for a point in importance calculation
+    top_k = 20
+    fis = np.sum(point_wise_importances, 0)
+    fis = np.argsort(-fis)[:top_k]
 
+    # Knowing which data points are best to be used, repeat process all over again
+    # Data that will store x and y values
+    clf = RandomForestClassifier(max_depth=2)
+    model_data = [[], []]
 
+    for loader in loaders:
+        # Load models and get/plot activations
+        get_acts(loader, models_1, model_data, ratio=args.ratio_1, mask=fis)
+        get_acts(loader, models_2, model_data, ratio=args.ratio_2, mask=fis)
 
-   
+    model_data[0] = np.array(model_data[0])
+    model_data[1] = np.array(model_data[1])
+
+    x_tr = model_data[0]
+    y_tr = (model_data[1] != args.ratio_1)
+    clf.fit(x_tr, y_tr)
+    train_acc = 100 * clf.score(x_tr, y_tr)
+    print("Accuracy using selected data", train_acc)
+
+    # Get accuracy with this method on unseen victim models
+    model_data = [[], []]
+
+    for loader in loaders:
+        # Load models and get/plot activations
+        get_acts(loader, models_victim_1, model_data,
+                 ratio=args.ratio_1, mask=fis)
+        get_acts(loader, models_victim_2, model_data,
+                 ratio=args.ratio_2, mask=fis)
+
+    model_data[0] = np.array(model_data[0])
+    model_data[1] = np.array(model_data[1])
+
+    x_te = model_data[0]
+    y_te = (model_data[1] != args.ratio_1)
+
+    print("Accuracy on unseen models", 100 * clf.score(x_te, y_te))
