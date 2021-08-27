374386e1b61c30cb722c44d56de1036b40e60407
Anshuman Suri <anshuman@email.virginia.edu>
2021-08-27 10:22:36 -0400

    Remove stuff

--- util/text_deltas_to_np.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import utils
-import sys
-
-src_path = sys.argv[1]
-dst_path = sys.argv[2]
-deltas  = utils.get_sensitivities(src_path)
-np_deltas = np.array(deltas)
-np.save(dst_path, np_deltas)
